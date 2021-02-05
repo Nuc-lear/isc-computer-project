bc5b883f5795a33c6dac720ba7442f852a691e91
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-02-05 21:10:05 +0530

    added new program

--- Kaprekar_Number.java
+++ Kaprekar_Number.java
@@ -3,15 +3,15 @@ class Kaprekar_Number
     static boolean kaprekar(int n)   
     {   
         int cpy=n;// copy the number
-        int p=1;
+        int p=1;// initialize
         while(cpy>0)
         {
             p=p*10;
             cpy=cpy/10;
         }
-        int s=n*n;//square of the number
-        int f=s/p;//first part
-        int l=s%p; // last part
+        int s=n*n;// square of the number
+        int f=s/p;// first part
+        int l=s%p;// last part
         if(f+l==n)
             return true;
         else
@@ -24,11 +24,12 @@ class Kaprekar_Number
         System.out.print("The Karpekar Numbers from 1 to 1000 are : ");
         int s=0;// initilaize
         for (int i=1; i<1000; i++)   
-            if (kaprekar(i))   {
+            if (kaprekar(i))   
+            {
                 System.out.print(i + " ");  
                 s=s+i;
             }
-       
+
         System.out.println("\nSum of all Karpekar Numbers from 1 to 1000 : "+s);
     }//end of main()
 }//end of class      
