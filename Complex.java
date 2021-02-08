7f0db240e9373e296a94db3ba3d05eadeeb16933
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-02-08 10:58:19 +0530

    added comments

--- Complex.java
+++ Complex.java
@@ -2,19 +2,20 @@ import java.util.*;
 class Complex
 {//start of class
     int x,y;//initialize
-    Complex()
-    {
+    Complex() 
+    { 
         x=0;
         y=0;
     }
+
     Complex(int xx, int yy)
-    {
+    { //parameterized constructor to assign value to data members
         x=xx;
         y=yy;
     }
 
     Complex Add(Complex obj)
-    {
+    { //calculates and returns the sum of the two complex numbers 
         Complex R= new Complex();
         R.x=this.x+obj.x;
         R.y=this.y+obj.y;
@@ -22,12 +23,13 @@ class Complex
     }
 
     void display()
-    {
+    { //to display the Complex Number
         if(y>=0)
             System.out.println(x+" + i"+y);
         else
             System.out.println(x+" -i"+Math.abs(y));
     }
+
     public static void main()
     {//start of main()
         Scanner sc= new Scanner(System.in);
@@ -37,15 +39,15 @@ class Complex
         System.out.print("Enter the real and imaginary part : ");
         int x2=sc.nextInt();
         int y2=sc.nextInt();
-        Complex C1= new Complex(x1,y1);
-        Complex C2= new Complex(x2,y2);
-        Complex C3= new Complex();
+        Complex C1= new Complex(x1,y1);//creating object
+        Complex C2= new Complex(x2,y2);//creating object
+        Complex C3= new Complex();//creating object
         C3=C1.Add(C2);
         System.out.println("First Complex Number : ");
-        C1.display();
+        C1.display();//function call
         System.out.println("Second Complex Number : ");
-        C2.display();
+        C2.display();//function call
         System.out.println("Final Complex Number : ");
-        C3.display();
+        C3.display();//function call
     }//end of main()
 }//end of class
